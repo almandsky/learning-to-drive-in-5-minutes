2c924fbc5a294fe561bad4286e80a5a9a4b494b8
Tawn Kramer <tawnkramer@gmail.com>
2019-02-13 09:19:51 -0800

    added helper scripts

---
+++ local_train_rl.sh
@@ -0,0 +1,1 @@
+python train.py --algo sac -n 50000 -vae logs/vae-32.pkl --local-control --donkey-name=sqr --broker=localhost
---
+++ resume_rl.sh
@@ -0,0 +1,1 @@
+python train.py --algo sac -n 50000 -vae logs/vae-32.pkl -i logs/sac/DonkeyVae-v0-level-3_13/DonkeyVae-v0-level-3_best.pkl --teleop --donkey-name=sqr --broker=localhost
